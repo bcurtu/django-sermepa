a2d00d1639081a1e6c259d28fa280d6cc941d6da
Bosco Curtu <bcurtu@gmail.com>
2015-04-12 00:13:41 +0200

    v 1.1.1 añadiendo el LICENSE

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sermepa',
-    version='1.1',
+    version='1.1.1',
     packages=['sermepa'],
     include_package_data=True,
     license='MIT License',  
