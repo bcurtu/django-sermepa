4fcde40db87f595636ea9a98729b14415ad80b09
Bosco Curtu <bcurtu@gmail.com>
2015-04-28 18:11:52 +0200

    package 1.1.2 with python3 compatibility

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-sermepa',
-    version='1.1.1',
+    version='1.1.2',
     packages=['sermepa'],
     include_package_data=True,
     license='MIT License',  
@@ -25,9 +25,9 @@ setup(
         'License :: OSI Approved :: MIT License', 
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.5',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
